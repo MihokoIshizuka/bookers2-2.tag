6045350a60fcf486e329820ad90f7cda8e40d364
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-05-09 13:32:52 +0000

    [add]2-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,7 +27,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
     else
       render request.referer
     end
