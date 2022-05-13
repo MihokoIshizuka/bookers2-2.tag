100623bc7a71a98ed831119fb5a121b0438251e3
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-05-13 06:18:03 +0000

    [add]2-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
@@ -9,6 +11,10 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create,:destroy]
   end
 
-  resources :users, only: [:show,:index,:edit,:update]
+  resources :users, only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
 end
